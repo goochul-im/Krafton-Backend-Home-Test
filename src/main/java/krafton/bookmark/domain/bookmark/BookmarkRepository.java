06d91ac1a3acab8114d298a73afc9f30eb5dd76e
goochul-im <goochul175465@gmail.com>
2025-10-29 15:27:58 +0900

    feat(BookmarkRepository): 모든 북마크 조회 추가
    테스트 완료

--- src/main/java/krafton/bookmark/domain/bookmark/BookmarkRepository.java
+++ src/main/java/krafton/bookmark/domain/bookmark/BookmarkRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
@@ -39,5 +40,11 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
             Pageable pageable);
 
     Optional<Bookmark> findByIdAndAuthor(Long id, Member author);
+
+    @Query("select b from Bookmark b " +
+            "left join fetch b.tag t " +
+            "where b.author = :author " +
+            "order by b.id desc")
+    List<Bookmark> findAllByAuthor(Member author);
 
 }
